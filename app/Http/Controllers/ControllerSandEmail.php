8b5b20200a5eb6e9303513113389c6227bd0d90c
Ajuda nunca foi tão fácil <salvadorbba@gmail.com>
2023-08-20 17:09:56 -0300

    Update ControllerSandEmail.php

--- app/Http/Controllers/ControllerSandEmail.php
+++ app/Http/Controllers/ControllerSandEmail.php
@@ -31,78 +31,78 @@ class ControllerSandEmail extends Controller
     {
 
 
-
-
-        $Response_Titulo = CobrancaTitulo::find($request->id);
-        $Bendeficiario = Beneficiario::find($Response_Titulo->beneficiario_id);
-        $Cliente = Cliente::find($Response_Titulo->cliente_id);
-        $Configuracao = ConfiguracaoEmail::find($Response_Titulo->parametros_bancos_id);
-
-
-        $Configuracao = ConfiguracaoEmail::where('parametros_bancos_id', $Response_Titulo->parametros_bancos_id)->first();
-
-        $Parametros = ParametrosBancos::select([
-            'modelo_id',
-            'client_secret',
-            'client_id',
-            'certificado',
-            'senha',
-            'client_id_bolecode',
-            'client_secret_bolecode',
-            'certificados_pix',
-            'certificados_extra',
-            'senha_certificado_pix',
-            'senha_certificado_extra',
-            'numerocontrato as id_beneficiario',
-            'carteira',
-            'id as parametros_bancos_id',
-            'system_unit_id',
-            'certificado_base64',
-            'certificado_pix_base64', 'identificacaoboletoempresa'
-        ])
-            ->where('id', '=', $Response_Titulo->parametros_bancos_id)
-            ->first();
-
-
-        $retorno = new stdClass();
-
-        $retorno->nome_cliente = $Cliente->nome;
-        $retorno->email_cliente = $Cliente->email;
-        $retorno->documento_cliente = $Cliente->documento;
-        $retorno->celular_cliente = $Cliente->telefone;
-        $retorno->endereco_cliente = $Cliente;
-        $retorno->valor = ClassGenerica::modeda_string($Response_Titulo->valor);
-        $retorno->localizador = $Parametros->identificacaoboletoempresa;
-        $retorno->vencimento = ClassGenerica::data_BR($Response_Titulo->data_vencimento);
-        $retorno->status_cobranca = $Response_Titulo->status;
-        $retorno->razao = $Bendeficiario->razao;
-        $retorno->cnpj = $Bendeficiario->cnpj;
-        //  $retorno->endereco_beneficiario = $ndereco_bendeficiario;
-        $retorno->data_atual = date('d/m/Y H:i:s');
-
-
-        $retorno->config = new stdClass();
-        $retorno->config->Host = $Configuracao->Host;
-        $retorno->config->SMTPAuth  = $Configuracao->SMTPAuth;
-        $retorno->config->Username  = $Configuracao->Username;
-        $retorno->config->Password = $Configuracao->Password;
-        $retorno->config->SMTPSecure = $Configuracao->SMTPSecure;
-        $retorno->config->Port  = $Configuracao->Port;
-        $retorno->config->Port  = $Configuracao->Port;
-        $retorno->config->Subject = $Configuracao->Subject;
-        $retorno->config->setFrom_name = $Configuracao->setFrom_name;
-        $retorno->config->setFrom = $Configuracao->setFrom;
-        $retorno->config->body = $Configuracao->body;
-
-        $retorno->boleto = new stdClass();
-
-        $retorno->boleto->seunumero = $Response_Titulo->seunumero;
-        $retorno->boleto->caminho_boleto = $Response_Titulo->caminho_boleto;
-        $retorno->boleto->DataDoProces = $Response_Titulo->DataDoProces;
-        $retorno->boleto->linhadigitavel = $Response_Titulo->linhadigitavel;
-        $retorno->boleto->codigobarras = $Response_Titulo->codigobarras;
-
         try {
+
+            $Response_Titulo = CobrancaTitulo::find($request->id);
+            $Bendeficiario = Beneficiario::find($Response_Titulo->beneficiario_id);
+            $Cliente = Cliente::find($Response_Titulo->cliente_id);
+            $Configuracao = ConfiguracaoEmail::find($Response_Titulo->parametros_bancos_id);
+
+
+            $Configuracao = ConfiguracaoEmail::where('parametros_bancos_id', $Response_Titulo->parametros_bancos_id)->first();
+
+            $Parametros = ParametrosBancos::select([
+                'modelo_id',
+                'client_secret',
+                'client_id',
+                'certificado',
+                'senha',
+                'client_id_bolecode',
+                'client_secret_bolecode',
+                'certificados_pix',
+                'certificados_extra',
+                'senha_certificado_pix',
+                'senha_certificado_extra',
+                'numerocontrato as id_beneficiario',
+                'carteira',
+                'id as parametros_bancos_id',
+                'system_unit_id',
+                'certificado_base64',
+                'certificado_pix_base64', 'identificacaoboletoempresa'
+            ])
+                ->where('id', '=', $Response_Titulo->parametros_bancos_id)
+                ->first();
+
+
+            $retorno = new stdClass();
+
+            $retorno->nome_cliente = $Cliente->nome;
+            $retorno->email_cliente = $Cliente->email;
+            $retorno->documento_cliente = $Cliente->documento;
+            $retorno->celular_cliente = $Cliente->telefone;
+            $retorno->endereco_cliente = $Cliente;
+            $retorno->valor = ClassGenerica::modeda_string($Response_Titulo->valor);
+            $retorno->localizador = $Parametros->identificacaoboletoempresa;
+            $retorno->vencimento = ClassGenerica::data_BR($Response_Titulo->data_vencimento);
+            $retorno->status_cobranca = $Response_Titulo->status;
+            $retorno->razao = $Bendeficiario->razao;
+            $retorno->cnpj = $Bendeficiario->cnpj;
+            //  $retorno->endereco_beneficiario = $ndereco_bendeficiario;
+            $retorno->data_atual = date('d/m/Y H:i:s');
+
+
+            $retorno->config = new stdClass();
+            $retorno->config->Host = $Configuracao->Host;
+            $retorno->config->SMTPAuth  = $Configuracao->SMTPAuth;
+            $retorno->config->Username  = $Configuracao->Username;
+            $retorno->config->Password = $Configuracao->Password;
+            $retorno->config->SMTPSecure = $Configuracao->SMTPSecure;
+            $retorno->config->Port  = $Configuracao->Port;
+            $retorno->config->Port  = $Configuracao->Port;
+            $retorno->config->Subject = $Configuracao->Subject;
+            $retorno->config->setFrom_name = $Configuracao->setFrom_name;
+            $retorno->config->setFrom = $Configuracao->setFrom;
+            $retorno->config->body = $Configuracao->body;
+
+            $retorno->boleto = new stdClass();
+
+            $retorno->boleto->seunumero = $Response_Titulo->seunumero;
+            $retorno->boleto->caminho_boleto = $Response_Titulo->caminho_boleto;
+            $retorno->boleto->DataDoProces = $Response_Titulo->DataDoProces;
+            $retorno->boleto->linhadigitavel = $Response_Titulo->linhadigitavel;
+            $retorno->boleto->codigobarras = $Response_Titulo->codigobarras;
+
+
             $mail = new PHPMailer(true);
 
             $mail->SMTPOptions = array(
@@ -129,37 +129,27 @@ class ControllerSandEmail extends Controller
             $mail->isHTML(true);
             $mail->Body =  utf8_decode($retorno->config->body);
             //  Adiciona o anexo
-
             $mail->addAttachment($retorno->boleto->caminho_boleto);
-
             // Define o destinatário
             $mail->addAddress($destinatario_email);
-
             // Envia o e-mail
             $mail->send();
-
             $parametros_bancos_id = $Response_Titulo->parametros_bancos_id;
-
             $cobranca_id =  $Response_Titulo->id;
             $mensagem = 'E-mail de Cobrança';
-
             $banco = $mensagem;
-
             $titulo = 'Envio Manual';
             $codigo = 200;
-
             $evento = new EventosBoletos();
-            // Preencher os campos do evento
 
+
+            // Preencher os campos do evento
             $evento->parametros_bancos_id = $Response_Titulo->parametros_bancos_id;
             $evento->cobranca_titulo_id =  $Response_Titulo->id;
             $evento->seunumero = $retorno->boleto->seunumero;
             $evento->linhadigitavel =   $retorno->boleto->linhadigitavel;
-
-
             $evento->mensagem = 'E-mail de Cobrança';
             $evento->codigo = 200;
-
             $evento->save();
 
             return response()->json([
@@ -183,7 +173,6 @@ class ControllerSandEmail extends Controller
             Log::error('Erro ao processar ControllerSandEmail: ' . $e->getMessage());
             return response()->json([
                 'Resposta' => [
-
                     'mensagem' => 'Erro alenviar email',
                 ],
             ], 404);
